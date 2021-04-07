6e9c95a8eaccc5607c16b5720b50394ee5b3f0f2
jack Zhao <a799949591@gmail.com>
2021-04-06 20:57:04 -0700

    Prototype1

---
+++ VGP233/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float CarSpeed = 10f;
+    public Camera followCamera;
+    //private Vector3 CarMove = new Vector3();
+    //private Vector3 CamMove = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CarMove.z = CarSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //vehicle movement
+        transform.Translate(Vector3.forward * Time.deltaTime * CarSpeed);
+        //followCamera.transform.z
+    }
+}
