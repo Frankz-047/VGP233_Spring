0c31485be1e74b284d82043a8e0e390156f954a4
jack Zhao <a799949591@gmail.com>
2021-04-14 13:08:44 -0700

    Assignement1

--- VGP233/Assets/Scripts/PlayerController.cs
+++ VGP233/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float CarSpeed = 10f;
-    public Camera followCamera;
-    //private Vector3 CarMove = new Vector3();
-    //private Vector3 CamMove = new Vector3();
-    // Start is called before the first frame update
-    void Start()
-    {
-        //CarMove.z = CarSpeed;
-    }
+    public float TurnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
         //vehicle movement
-        transform.Translate(Vector3.forward * Time.deltaTime * CarSpeed);
-        //followCamera.transform.z
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * CarSpeed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * TurnSpeed * horizontalInput);
+        if (forwardInput != 0)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * horizontalInput);
+        }
     }
 }
---
+++ Wuji_Zhao_Assignment_1/Wuji_Zhao_Assignment_1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float propellerSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * propellerSpeed * Time.deltaTime);
+    }
+}
