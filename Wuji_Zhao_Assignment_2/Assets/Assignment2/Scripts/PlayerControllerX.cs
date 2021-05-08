0f90a781738cca5aad035902d1af67bf5fe26e7b
ETavaresLCIDemo <etavareslcidemo@gmail.com>
2021-05-07 18:20:54 -0700

    Assignment 2 commented.

--- Wuji_Zhao_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
+++ Wuji_Zhao_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -9,6 +9,8 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ERROR: -1. The last task was to just allow the player to spawn a dog after some time.
+        //You are allowing the player to spawn every time.
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
